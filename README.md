bccc679ce02982be83872d0e0c5d2afedba73580
K@zuki <k@zuki.dev>
2023-05-19 01:18:48 +0900

    change: refactor

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# env-revealer
-Container that returns environment variables that can be referenced.
+# revealer
 
-## Configuration
+The repository for manage a container image of HTTP server that returns the source IP, request headers, request body, and query string.
+It is used to check request data and to isolate problems when you want to check whether a request is correctly sent to a server via VPN or Proxy, but cannot be checked by the server in question.
+
+You can check the operation at [revealer.zuki.dev](https://revealer.zuki.dev).
+
+## Run
+
+Using docker:
+
+```sh
+docker run -p 8080:8080 --rm -it ghcr.io/corrupt952/revealer
+```
+
+## Configurations
+
+### Environment variables
+
 * PORT ... Start up on the specified por.t Set with environment variable. (Default: 8080)
--- main.go
+++ main.go
@@ -1,21 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		for _, pair := range os.Environ() {
-			fmt.Fprintln(w, pair)
+/**
+ * Middleware
+ */
+type Log struct {
+	Time       string `json:"time"`
+	RemoteAddr string `json:"remote_addr"`
+	Method     string `json:"method"`
+	Proto      string `json:"proto"`
+	Host       string `json:"host"`
+	Uri        string `json:"uri"`
+	Query      string `json:"query"`
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := Log{
+			Time:       time.Now().Format("2006-01-02 15:04:05"),
+			RemoteAddr: r.RemoteAddr,
+			Method:     r.Method,
+			Host:       r.Host,
+			Proto:      r.Proto,
+			Uri:        r.RequestURI,
+			Query:      r.URL.Query().Encode(),
 		}
+		json, err := json.Marshal(log)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("%s\n", json)
+		next.ServeHTTP(w, r)
 	})
+}
+
+/**
+ * Handlers
+ */
+type Response struct {
+	RemoteAddr string      `json:"remote_addr"`
+	Headers    http.Header `json:"headers"`
+	Body       string      `json:"body"`
+	Query      string      `json:"query"`
+}
+
+func (r *Response) String() string {
+	json, err := json.MarshalIndent(r, "", "  ")
+	if err != nil {
+		return fmt.Sprintf("{\"error\": \"%s\"}", err)
+	}
+	return string(json)
+}
+
+func buildResponse(r *http.Request) (Response, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return Response{}, err
+	}
+	response := Response{
+		RemoteAddr: r.RemoteAddr,
+		Headers:    r.Header,
+		Body:       string(body),
+		Query:      r.URL.Query().Encode(),
+	}
+	return response, nil
+}
+
+func main() {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response, err := buildResponse(r)
+		if err != nil {
+			fmt.Fprintf(w, "{\"error\": \"%s\"}\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", response.String())
+	})
+	http.Handle("/", loggingMiddleware(handler))
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	http.ListenAndServe(":" + port, nil)
+	log.Printf("Listening on port %s", port)
+	http.ListenAndServe(":"+port, nil)
 }
